a583866dff216298a24d76c2cfed2c5c27ccb666
Pharez <pharezoyelade@gmail.com>
2026-04-26 18:56:37 +0100

    config(utility): create toogle completetion utility function

---
+++ src/lib/habits.ts
@@ -0,0 +1,8 @@
+import { Habit } from "@/types/habit";
+
+export function toggleHabitCompletion(habit: Habit, date: string): Habit {
+  const alreadyCompleted = habit.completions.includes(date)
+  const updatedCompletions = alreadyCompleted ? habit.completions.filter((d) => d !== date) : [...new Set([...habit.completions, date])];
+
+  return {...habit, completions: updatedCompletions}
+}
